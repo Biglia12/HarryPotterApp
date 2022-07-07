6e7243080e862b288b4ed13b998026e9ec37d1f9
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2022-07-07 18:49:35 -0300

    Initial commit

---
+++ app/src/main/java/com/kotlin/harrypotterapp/activities/SpashActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.harrypotterapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.harrypotterapp.R
+
+class SpashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spash)
+    }
+}
