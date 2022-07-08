28f371721e34828268e97e7cb274822fa2b1f73e
Ezequiel Biglia <ezequielbiglia12@gmail.com>
2022-07-07 21:01:12 -0300

    se agrega splas

---
+++ app/src/main/java/com/kotlin/harrypotterapp/activities/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.kotlin.harrypotterapp.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.kotlin.harrypotterapp.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    val time: Long = 2000 // esta variable hara referencia al tiempo que tarde el splash
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, time)
+
+    }
+}
